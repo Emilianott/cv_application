a8a35ed3b3c0cc74dce8f962412367381377e099
Roberto Tejeda <emilianott19@gmail.com>
2023-07-27 01:52:10 -0400

    Adding Forms

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,121 +1,129 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 export const Form = (): JSX.Element => {
-    const [basicInfo, setBasicInfo] = useState<boolean>(false)
+    const [basicInfo, setBasicInfo] = useState<boolean>(true)
+    const [education, setEducation] = useState<boolean>(false)
 
-    const handleBasicInfo=():void=>{
-      setBasicInfo(!basicInfo)
+    const handleBasicInfo = (value: number): void => {
+        if (value === 1) {
+            setBasicInfo(!basicInfo)
+        } else if (value === 2) {
+            setEducation(!education)
+        }
     }
 
-    
-
-
-
     return (
-        <aside className='w-2/6 border border-black m-8 bg-gray-800 text-white'>
-            <div className='w-full h-auto flex flex-col p-3'>
-
-              
-                <h2 onClick={handleBasicInfo} className='font-extrabold pl-10 text-xl mb-2 hover:scale-[1.3] w-full origin-left pointer hover:p-x cursor-pointer transition ease-in-out'>
+        <aside className='w-2/6 border border-black m-8 bg-gray-800 text-white shadow'>
+            <div className='w-full h-auto flex flex-col p-3 justify-center items-center '>
+                <h2
+                    onClick={() => {
+                        handleBasicInfo(1)
+                    }}
+                    className='font-extrabold mt-8 text-center text-xl mb-2 hover:scale-[1.3] w-full  pointer hover:p-x cursor-pointer transition ease-in-out'>
                     Basic Information
                 </h2>
-                <hr className='bg-black border border-yellow-400 w-3/4 ml-8' />
+                <hr className='bg-black border border-yellow-400 w-3/4 ' />
 
-                {basicInfo && 
-                <form
-                    className='w-full h-auto flex flex-col   '
-                    onSubmit={(e) => e.preventDefault()}>
-                    <label
-                        htmlFor='name'
-                        className='mt-4 pl-5 ml-1 font-extrabold  '>
-                        Name
-                    </label>
-                    <input
-                        id='name'
-                        type='text'
-                        placeholder='Enter your name '
-                        className='border border-black w-1/2 rounded-xl pl-3 ml-4 '
-                    />
-                    <label
-                        htmlFor='profession'
-                        className='mt-6 pl-5 font-extrabold  '>
-                        Profession
-                    </label>
-                    <input
-                        id='profession'
-                        type='text'
-                        placeholder='Enter your profession '
-                        className='border border-black w-1/2 rounded-xl pl-1 ml-4 '
-                    />
-                    <label
-                        htmlFor='aboutyou '
-                        className='mt-6 pl-5 font-extrabold '>
-                        About you
-                    </label>
-                    <textarea
-                        className='w-3/4 mb-5 ml-4 '
-                        placeholder="Briefly describe yourself"
-                        name='aboutyou'
-                        id='aboutyou'
-                        cols={1}
-                        rows={2}></textarea>
-                    <div className=" flex justify-center items-center mt-10 mb-8">
-                    <button className='w-1/2 border border-black  hover:scale-110 hover:bg-yellow-400 bg-yellow-300 text-black mt-10 transition ease-in-out  rounded-xl'>
-                        Save
-                    </button>
-                    </div>
-                </form>
-                  }
+                {basicInfo && (
+                    <form
+                        className={`w-full h-auto flex flex-col justify-center items-center text-center `}
+                        onSubmit={(e) => e.preventDefault()}>
+                        <label
+                            htmlFor='name'
+                            className='mt-4   font-extrabold  '>
+                            Name
+                        </label>
+                        <input
+                            id='name'
+                            type='text'
+                            placeholder='Enter your name '
+                            className='border border-black w-1/2 rounded-xl text-center text-black'
+                        />
+                        <label
+                            htmlFor='profession'
+                            className='mt-6  font-extrabold  '>
+                            Profession
+                        </label>
+                        <input
+                            id='profession'
+                            type='text'
+                            placeholder='Enter your profession '
+                            className='border border-black w-1/2 rounded-xl text-center text-black'
+                        />
+                        <label
+                            htmlFor='aboutyou '
+                            className='mt-6  font-extrabold '>
+                            About you
+                        </label>
+                        <textarea
+                            className='w-1/2 mb-5  text-center text-black'
+                            placeholder='Briefly describe yourself '
+                            name='aboutyou'
+                            id='aboutyou'
+                            cols={1}
+                            rows={2}></textarea>
+                        <div className=' flex justify-center items-center w-full mt-10 mb-8'>
+                            <button className='w-1/2 border border-black  hover:scale-110 hover:bg-yellow-400 bg-yellow-300 text-black mt-6 transition ease-in-out  rounded-xl'>
+                                Save
+                            </button>
+                        </div>
+                    </form>
+                )}
             </div>
-            <div className='w-full h-auto'>
-                <h2  className='font-extrabold  pl-10 text-xl mt-5 mb-2  hover:scale-[1.3] w-full origin-left pointer hover:p-x cursor-pointer transition ease-in-out'>
+            <div className='w-full h-auto p-3 flex flex-col justify-center items-center '>
+                <h2
+                    onClick={() => {
+                        handleBasicInfo(2)
+                    }}
+                    className='font-extrabold text-center  text-xl mt-5 mb-2  hover:scale-[1.3] w-full  pointer hover:p-x cursor-pointer transition ease-in-out'>
                     Education
                 </h2>
-                <hr className='bg border border-yellow-400 w-3/4 ml-8' />
-                <form
-                    className='mt-5 flex flex-col'
-                    action=''>
-                    <label
-                        htmlFor='institute'
-                        className='mt-4 pl-8 ml-1 font-extrabold  '>
-                        Name of institution
-                    </label>
-                    <input
-                        id='institute'
-                        type='text'
-                        placeholder='Name of the institution'
-                        className='border border-black w-1/2 rounded-xl pl-1 ml-7 '
-                    />
+                <hr className='bg border border-yellow-400 w-3/4 ' />
+                {education && (
+                    <form
+                        className='mt-5 flex flex-col w-full justify-center items-center'
+                        action=''>
+                        <label
+                            htmlFor='institute'
+                            className='mt-4  ml-1 font-extrabold  '>
+                            Name of institution
+                        </label>
+                        <input
+                            id='institute'
+                            type='text'
+                            placeholder='Name of the institution'
+                            className='border border-black w-1/2 rounded-xl text-black text-center'
+                        />
 
-                    <label
-                        htmlFor='Diploma'
-                        className='mt-6 pl-8 ml-1 font-extrabold  '>
-                        Diploma
-                    </label>
-                    <input
-                        id='Diploma'
-                        type='text'
-                        placeholder='Enter your diploma'
-                        className='border border-black w-1/2 rounded-xl pl-5 ml-7 '
-                    />
-                    <label
-                        htmlFor='date'
-                        className='mt-6 pl-8 ml-1 font-extrabold  '>
-                        Finished Date
-                    </label>
-                    <input
-                        id='date'
-                        type='date'
-                        placeholder='Enter your name '
-                        className='border border-black w-1/2 rounded-xl pl-5 ml-7 text-black'
-                    />
-                    <div className=" flex justify-center items-center mt-16">
-
-                    <button className='w-1/2 border border-black   mb-5 hover:scale-110 bg-yellow-300 text-black hover:bg-yellow-400 transition ease-in-out  rounded-xl'>
-                        Save
-                    </button>
-                    </div>
-                </form>
+                        <label
+                            htmlFor='Diploma'
+                            className='mt-6  ml-1 font-extrabold  '>
+                            Diploma
+                        </label>
+                        <input
+                            id='Diploma'
+                            type='text'
+                            placeholder='Enter your diploma'
+                            className='border border-black w-1/2 rounded-xl  text-black text-center'
+                        />
+                        <label
+                            htmlFor='date'
+                            className='mt-6   font-extrabold  '>
+                            Finished Date
+                        </label>
+                        <input
+                            id='date'
+                            type='date'
+                            placeholder='Enter your name '
+                            className='border border-black w-1/2 rounded-xl   text-black text-center'
+                        />
+                        <div className=' flex justify-center w-full items-center mt-16'>
+                            <button className='w-1/2 border border-black   mb-5 hover:scale-110 bg-yellow-300 text-black hover:bg-yellow-400 transition ease-in-out  rounded-xl'>
+                                Save
+                            </button>
+                        </div>
+                    </form>
+                )}
             </div>
         </aside>
     )
